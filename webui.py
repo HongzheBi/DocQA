67aa6979f4d041bd8e26342f5a638b88e4b6f955
HongzheBi <2250347390@qq.com>
2023-05-22 18:06:08 +0800

    webui

--- webui.py
+++ webui.py
@@ -38,46 +38,51 @@ def initialize():
 
 initialize()   
 
-def chat(query, choice, upload, vector_store_path):
+def chat(query, choice, filepath):
     if choice == "上传文件/文件夹":
-        retriever = doc_qa.init_knowledge_vector_store(upload) 
+        retriever = doc_qa.init_knowledge_vector_store(filepath) 
     else:
-        retriever = doc_qa.load_VectorDB(vector_store_path)
+        retriever = doc_qa.load_VectorDB(filepath)
     docs = retriever.get_relevant_documents(query)
     prompt = doc_qa.generate_prompt(query, docs)
     response = doc_qa.llm._call(prompt=prompt)
     return response
 
 # 初始化时不显示这两个组件
-upload = gr.components.File(type="file", label="上传文件/文件夹", visible=False) 
-vector_store_path = gr.components.File(type="file", label="选择知识库文件夹", visible=False) 
-
+#upload = gr.components.File(type="file", label="上传文件/文件夹", visible=False) 
+#vector_store_path = gr.components.File(type="file", label="选择知识库文件夹", visible=False) 
+'''
 # 定义显示upload组件的方法  
 def show_upload():   
-    upload.visible = True
-    vector_store_path.visible = False
+    upload = gr.components.File(type="file", label="上传文件/文件夹", visible=True) 
+    vector_store_path = gr.components.File(type="file", label="选择知识库文件夹", visible=False)
+    return upload, vector_store_path 
+
     
 # 定义显示load_embedding组件的方法  
 def show_load_embedding():
-    upload.visible = False
-    vector_store_path.visible = True  
+    upload = gr.components.File(type="file", label="上传文件/文件夹", visible=False) 
+    vector_store_path = gr.components.File(type="file", label="选择知识库文件夹", visible=True)
+    return  upload, vector_store_path
 
 # 根据选择调用对应方法  
 def visible(choice): 
     if choice == "上传文件/文件夹":
-        show_upload()
+        return show_upload()
     else:
-        show_load_embedding()
+        return show_load_embedding()
+'''
         
-source_choicer = gr.components.Radio(choices=["上传文件/文件夹", "使用已有知识库文件/文件夹"],  
-                                     label="选择知识库来源",
-                                     onchange=visible)    
+source_choicer = gr.components.Radio(choices=["上传文件/文件夹", "使用已有知识库"],  
+                                     label="选择知识库来源", interactive=True)
+
+filepath =  gr.components.File(type="file", label="上传文件/文件夹", visible=True) 
 
 query = gr.components.Textbox(lines=1, label="输入查询")
 
 outputs = gr.outputs.Textbox(label="回复")  
 
-gr.Interface(fn=chat, inputs=[source_choicer, upload, vector_store_path, query],
+gr.Interface(fn=chat, inputs=[source_choicer, filepath, query],
       outputs=outputs, title="基于本地知识库检索和LLM轻量化微调的问答系统", style=style).launch(  
             server_name='0.0.0.0',                         
             server_port=7860,
