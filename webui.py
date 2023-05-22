c93d3203480b9618e1fe5fbe9647a90ee0b380b9
HongzheBi <2250347390@qq.com>
2023-05-22 20:50:31 +0800

    webui

--- webui.py
+++ webui.py
@@ -76,14 +76,13 @@ def visible(choice):
 source_choicer = gr.components.Radio(choices=["上传文件/文件夹", "使用已有知识库"],  
                                      label="选择知识库来源", interactive=True)
 
-filepath =  gr.File(file_types=['.txt', '.md', '.docx', '.pdf'],
-                                            file_count="multiple", label="上传文件/文件夹", visible=True) 
+filepath =  gr.File(file_types=['.txt', '.md', '.docx', '.pdf'], label="上传文件/文件夹", visible=True) 
 
 query = gr.components.Textbox(lines=1, label="输入查询")
 
 outputs = gr.outputs.Textbox(label="回复")  
 
-gr.Interface(fn=chat, inputs=[query, source_choicer, filepath],
+gr.Interface(fn=chat, inputs=[query, source_choicer, filepath.name],
       outputs=outputs, title="基于本地知识库检索和LLM轻量化微调的问答系统", style=style).launch(  
             server_name='0.0.0.0',                         
             server_port=7860,
